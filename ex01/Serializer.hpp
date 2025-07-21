21620122ba6c3ee0848ae1ca74c562354ff3dd8a
mfunakos <orz.home35@gmail.com>
2025-07-21 13:23:39 +0900

    ex01: 使用しないものをプライベートにおいた

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -16,15 +16,15 @@
 
 class Serializer {
 	public:
+		static	uintptr_t serialize(Data* ptr);
+		static	Data* deserialize(uintptr_t raw);
+
+	private:
 		Serializer();
 		Serializer(const std::string& name, int grade);
 		Serializer& operator=(const Serializer& copy);
 		Serializer(const Serializer& copy);
 		~Serializer();
-
-		static	uintptr_t serialize(Data* ptr);
-		static	Data* deserialize(uintptr_t raw);
-	private:
 };
 
 // ------------------------------------------------
